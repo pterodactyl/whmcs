d9a2ef07e55a6ba88b93dbfcc7fc4172b3c8f90d
Stepan Fedotov <TrixterTheTux@users.noreply.github.com>
2020-09-29 12:55:22 +0300

    Support WHMCS v8

--- pterodactyl/pterodactyl.php
+++ pterodactyl/pterodactyl.php
@@ -236,10 +236,20 @@ function pterodactyl_TestConnection(array $params) {
     ];
 }
 
+function random($length) {
+    if (class_exists("\Illuminate\Support\Str")) {
+        return \Illuminate\Support\Str::random($length);
+    } else if (function_exists("str_random")) {
+        return str_random($length);
+    } else {
+        throw new \Exception("Unable to find a valid function for generating random strings");
+    }
+}
+
 function pterodactyl_GenerateUsername($length = 8) {
     $returnable = false;
     while (!$returnable) {
-        $generated = str_random($length);
+        $generated = random($length);
         if (preg_match('/[A-Z]+[a-z]+[0-9]+/', $generated)) {
             $returnable = true;
         }
